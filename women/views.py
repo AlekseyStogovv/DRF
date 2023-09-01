812d9ff9e1f61f1b51728c60c8de5ce65a4caf1b
AlekseyStogovv <astogovv@gmail.com>
2023-09-01 15:29:37 +0300

    change serialazer on ModelSerializer

--- women/views.py
+++ women/views.py
@@ -1,26 +1,31 @@
 from django.forms import model_to_dict
 from django.shortcuts import render
-from rest_framework import generics, status
+from rest_framework import generics, status, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Women
 from .serializers import WomenSerializer
 
-
-class WomenAPIList(generics.ListCreateAPIView):
+class WomenViewSet(viewsets.ModelViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
 
-class WomenAPIUpdate(generics.UpdateAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
 
-
-class WomenAPIDetailView(generics.RetrieveUpdateAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+# class WomenAPIList(generics.ListCreateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+#
+#
+# class WomenAPIUpdate(generics.UpdateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+#
+#
+# class WomenAPIDetailView(generics.RetrieveUpdateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
 
 # class WomenAPIView(generics.ListAPIView):
 #     queryset = Women.objects.all()
