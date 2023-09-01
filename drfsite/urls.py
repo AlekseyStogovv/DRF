479037ec6fe17305e96c24ae2f6b74752ed19482
AlekseyStogovv <astogovv@gmail.com>
2023-09-01 17:18:06 +0300

    add quryset and custom_router

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -3,8 +3,25 @@ from django.contrib import admin
 from django.urls import path, include
 from women.views import *
 
-router = routers.SimpleRouter()
-router.register(r'women', WomenViewSet)
+
+class MyCustomRouter(routers.SimpleRouter):
+    routers = [
+        routers.Route(url=r'^{prefix}$',
+                      mapping={'get': 'list'},
+                      name='{basename}-list',
+                      detail=False,
+                      initkwargs={'suffix': 'list'}),
+        routers.Route(url=r'^{prefix}/{lookup}$',
+                      mapping={'get': 'retrieve'},
+                      name='{basename}-detail',
+                      detail=True,
+                      initkwargs={'suffix': 'Detail'}),
+    ]
+
+router = MyCustomRouter()
+router.register(r'women', WomenViewSet, basename='women')
+print(router.urls)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
